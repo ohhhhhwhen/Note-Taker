8bc3f777b321907e74cbf3335e5eef211c3cd04e
Owen Chanthala <owenchanthala@gmail.com>
2020-02-12 10:05:13 -0800

    running

---
+++ routes/apiRoutes.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const noteTables = require('../db/db');
+
+module.exports = app => {    
+    app.get("/api/notes", (req, res) => res.json(noteTables));
+    
+    app.post("/api/notes", (req, res) => {
+      const newNotes = req.body;
+      noteTables.push(newNotes);
+      res.json(newNotes);
+    });
+
+    app.delete('/api/notes/:id', (req, res) => {
+        delete noteTables[id];
+        tables = tables.slice(id);
+        return res.json(noteTables);
+    });
+};
